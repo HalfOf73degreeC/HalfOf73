48c3dfbedb435fd322ea3b1718dc9ace843aa375
Asdnji <java008.project@gmail.com>
2018-06-02 17:26:41 +0800

    NEWS:新增JQuery程式(尚未測試)

---
+++ src/main/java/news/getNewsPage.java
@@ -0,0 +1,41 @@
+package news;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+/**
+ * Servlet implementation class getMemberPage
+ */
+@WebServlet("/nwes/getMemberPage")
+public class getNewsPage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		NewsDaoImpl mdao = new NewsDaoImpl();
+		// mdao.setPageNo(pageNo);
+		List<NewsBean_HO73> list = mdao.getAllNews();
+		Gson gson = new Gson();
+		String gString = gson.toJson(list);
+		response.setContentType("application/json; charset=UTF8");
+		try (PrintWriter out = response.getWriter();) {
+			out.println(gString);
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+
+}
