5a8453ee80a724416cf60493e215c8edea663394
azhgit <ananhuang781@gmail.com>
2018-07-02 11:10:31 +0800

    修改義賣上架之後台畫面
    1.上架頁面加入新的裁切視窗
    2.裁切功能尚未加入

--- src/main/webapp/foundationView/js/previewImg.js
+++ src/main/webapp/foundationView/js/previewImg.js
@@ -16,14 +16,53 @@ $(document).ready(function(){
             $("#preview_img").attr('src', e.target.result);
             $uploadCrop.croppie('bind', {   
                 url: e.target.result   
-            });   
+            });  
+  
          }
      
          reader.readAsDataURL(input.files[0]);
        }
      
      }
-     $uploadCrop = $("#preview_img").croppie({
+      $uploadCrop = $("#preview_img").croppie({
+    	  enableExif: true,
+    	  viewport: {
+    		  width: 360,
+    		  height: 360,
+    		  type: 'circle'
+    	  },
+    	  boundary: {
+    		  width: 555,
+    		  height: 400
+    	  }
+      });
+});
+$(document).ready(function(){
+    $("#input_img2").change(function(){
+
+        readURL(this);
+      
+      });
+
+      function readURL(input){
+      
+        if(input.files && input.files[0]){
+      
+          var reader = new FileReader();
+      
+          reader.onload = function (e) {
+      
+            $("#preview_img2").attr('src', e.target.result);
+            $uploadCrop2.croppie('bind', {   
+                url: e.target.result  
+            });   
+         }
+     
+         reader2.readAsDataURL(input.files[0]);
+       }
+     
+     }
+     $uploadCrop2 = $("#preview_img2").croppie({
          enableExif: true,
          viewport: {
              width: 200,
@@ -31,8 +70,15 @@ $(document).ready(function(){
    	        type: 'circle'
          },
          boundary: {
-             width: 300,
+             width: 280,
              height: 250
          }
      });
+
+$("#crop_img").click(function(){
+    
+});
+$("#final_img").click(function(){
+    
+});
 });
